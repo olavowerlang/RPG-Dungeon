7432c2199c66a5d0445c9fbd8e6e77093b8b039e
olavowerlang <olavomw@gmail.com>
2025-05-19 11:31:25 -0300

    fucking collider.

--- Assets/Scripts/SkeletonFighter.cs
+++ Assets/Scripts/SkeletonFighter.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class SkeletonFighter : MonoBehaviour
@@ -8,49 +10,49 @@ public class SkeletonFighter : MonoBehaviour
 
     [Header("Dash Tracking")]
     [Range(0f, 1f)]
-    [SerializeField] float dashTrackingFraction = 0.5f;
+    [SerializeField] private float dashTrackingFraction = 0.5f;
 
     [Header("Distances")]
-    [SerializeField] float orbitRadius  = 3.0f;
-    [SerializeField] float attackRange  = 1.2f;
+    [SerializeField] private float orbitRadius  = 3.0f;
+    [SerializeField] private float attackRange  = 1.2f;
 
     [Header("Times")]
-    [SerializeField] Vector2 guardTimeRange = new (1.3f, 2.5f);
-    [SerializeField] float dashPrepTime = 0.35f;
-    [SerializeField] float cooldownTime = 0.7f;
+    [SerializeField] private Vector2 guardTimeRange = new (1.3f, 2.5f);
+    [SerializeField] private float dashPrepTime = 0.35f;
+    [SerializeField] private float cooldownTime = 0.7f;
 
     [Header("Speed")]
-    [SerializeField] float approachSpeed = 1.4f;
-    [SerializeField] float orbitSpeed    = 1.2f;
-    [SerializeField] float dashSpeed     = 2.2f;
+    [SerializeField] private float approachSpeed = 1.4f;
+    [SerializeField] private float orbitSpeed    = 1.2f;
+    [SerializeField] private float dashSpeed     = 2.2f;
 
     [Header("Refs")]
-    [SerializeField] GameObject swordHitbox;
-    [SerializeField] EnemyAnimator enemyAnim;
-    [SerializeField] Transform sfVisual;
+    [SerializeField] private GameObject swordHitbox;
+    [SerializeField] private EnemyAnimator enemyAnim;
+    [SerializeField] private Transform sfVisual;
 
-    public Rigidbody2D Rb { get; private set; }
-    Transform player;
-    Vector2 dashTarget;
-    float _timer, _guardTimer;
+    private Rigidbody2D Rb { get; set; }
+    private Transform _player;
+    private Vector2 _dashTarget;
+    private float _timer, _guardTimer;
 
-    float dashDuration;
-    float dashElapsed;
-    bool dashTargetLocked;
+    private float _dashDuration;
+    private float _dashElapsed;
+    private bool _dashTargetLocked;
 
     public bool IsWalking => Rb.velocity != Vector2.zero;
 
-    void Awake()
+    private void Awake()
     {
-        Rb     = GetComponent<Rigidbody2D>();
-        player = GameObject.FindWithTag("Player").transform;
+        Rb = GetComponent<Rigidbody2D>();
+        _player = GameObject.FindWithTag("Player").transform;
         swordHitbox.SetActive(false);
         ResetGuardTimer();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        Vector2 toPlayer = player.position - transform.position;
+        Vector2 toPlayer = _player.position - transform.position;
         float dist = toPlayer.magnitude;
 
         switch (_state)
@@ -78,29 +80,29 @@ public class SkeletonFighter : MonoBehaviour
             case S.DashMove:
                 {                 
                     Vector2 currentPos = (Vector2)transform.position;
-                    Vector2 playerPos = (Vector2)player.position;
+                    Vector2 playerPos = (Vector2)_player.position;
 
-                    dashElapsed += Time.fixedDeltaTime;
-                    if (!dashTargetLocked)
+                    _dashElapsed += Time.fixedDeltaTime;
+                    if (!_dashTargetLocked)
                     {
-                        if (dashElapsed <= dashDuration * dashTrackingFraction)
+                        if (_dashElapsed <= _dashDuration * dashTrackingFraction)
                         {                          
-                            dashTarget = playerPos;
+                            _dashTarget = playerPos;
                         }
                         else
                         {
                             Vector2 dir = (playerPos - currentPos).normalized;
-                            dashTargetLocked = true;
-                            dashTarget = playerPos - dir * attackRange;
+                            _dashTargetLocked = true;
+                            _dashTarget = playerPos - dir * attackRange;
                         }
                     }
 
-                    Vector2 toTarget = dashTarget - currentPos;
+                    Vector2 toTarget = _dashTarget - currentPos;
                     Vector2 movement = toTarget.normalized * dashSpeed;
                     Vector2 futurePos = currentPos + movement * Time.fixedDeltaTime;
                     Rb.velocity = movement;
 
-                    if (Vector2.Dot(toTarget, dashTarget - futurePos) <= 0f)
+                    if (Vector2.Dot(toTarget, _dashTarget - futurePos) <= 0f)
                         EnterHit();
                 }
                 break;
@@ -130,50 +132,50 @@ public class SkeletonFighter : MonoBehaviour
     
     }
 
-    void EnterApproach()
+    private void EnterApproach()
     {
         _state = S.Approach;
     }
 
-    void EnterOrbit()
+    private void EnterOrbit()
     {
         _state = S.Orbit;
         ResetGuardTimer();
     }
 
-    void EnterDashPrep()
+    private void EnterDashPrep()
     {
         _state = S.DashPrep;
         _timer = dashPrepTime;
     }
 
-    void EnterDashMove()
+    private void EnterDashMove()
     {
         _state = S.DashMove;
 
-        float initialDist = Vector2.Distance(transform.position, player.position);
-        dashDuration = initialDist / dashSpeed;
-        dashElapsed = 0f;
-        dashTargetLocked = false;
+        var initialDist = Vector2.Distance(transform.position, _player.position);
+        _dashDuration = initialDist / dashSpeed;
+        _dashElapsed = 0f;
+        _dashTargetLocked = false;
 
-        dashTarget = player.position;
+        _dashTarget = _player.position;
     }
 
-    void EnterHit()
+    private void EnterHit()
     {
         _state = S.Hit;
         swordHitbox.SetActive(true);
         enemyAnim.PlaySfAttack();
     }
 
-    void EnterCooldown()
+    private void EnterCooldown()
     {
         _state = S.Cooldown;
         _timer = cooldownTime;
         swordHitbox.SetActive(false);
     }
 
-    void ResetGuardTimer() =>
+    private void ResetGuardTimer() =>
         _guardTimer = Random.Range(guardTimeRange.x, guardTimeRange.y);
 
     public void OnAttackAnimationEnd()
@@ -185,10 +187,8 @@ public class SkeletonFighter : MonoBehaviour
 
     public void DefineSfSpriteDirection()
     {
-        bool faceLeft;
+        var faceLeft = _player.position.x < transform.position.x;
         
-            faceLeft = player.position.x < transform.position.x;
-
         var sc = sfVisual.localScale;
         sc.x = faceLeft ? -1.75f : 1.75f;
         sfVisual.localScale = sc;
